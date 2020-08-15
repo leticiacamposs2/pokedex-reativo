4e809e4ef9b61843ad150df94b3aa2ba9b37672c
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-08-15 20:10:39 -0300

    iniciando um projeto webflux

---
+++ src/main/java/com/leticiacamposs2/podedex/PodedexApplication.java
@@ -0,0 +1,13 @@
+package com.leticiacamposs2.podedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodedexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leticiacamposs2/podedex/PodedexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leticiacamposs2.podedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
