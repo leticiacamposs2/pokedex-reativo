45c211d7a41f409e37d61c7b1e6773061e5034c3
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-08-15 23:16:28 -0300

    metodo put e delete

--- src/main/java/com/leticiacamposs2/pokedex/controller/PokemonController.java
+++ src/main/java/com/leticiacamposs2/pokedex/controller/PokemonController.java
@@ -31,4 +31,35 @@ public class PokemonController {
         return repository.save(pokemon);
     }
 
+    @PutMapping("{id}")
+    public Mono<ResponseEntity<Pokemon>> updatePokemon(@PathVariable(value="id")
+                                                       String id,
+                                                       @RequestBody Pokemon pokemon) {
+        return repository.findById(id)
+                .flatMap(existingPokemon -> {
+                    existingPokemon.setNome(pokemon.getNome());
+                    existingPokemon.setCategoria(pokemon.getCategoria());
+                    existingPokemon.setHabilidade(pokemon.getHabilidade());
+                    existingPokemon.setPeso(pokemon.getPeso());
+                    return repository.save(existingPokemon);
+                })
+                .map(updatePokemon -> ResponseEntity.ok(updatePokemon))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("{id}")
+    public Mono<ResponseEntity<Void>> deletePokemon(@PathVariable(value = "id") String id) {
+        return repository.findById(id)
+                .flatMap(existingPokemon ->
+                        repository.delete(existingPokemon)
+                                .then(Mono.just(ResponseEntity.ok().<Void>build()))
+                )
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping
+    public Mono<Void> deleteAllPokemons() {
+        return repository.deleteAll();
+    }
 }
+
